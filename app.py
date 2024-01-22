cde68859c39be29584272bcef755b98c903e1bc2
BEAST\Prashanth <nagaprashanth0006@gmail.com>
2024-01-22 11:31:58 +0530

    Add favicon

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import hug
-from flask import Flask
+from flask import Flask, send_from_directory
 import os
 import base64
 import logging
@@ -24,3 +24,16 @@ def greet(name: str, key: str = "fake"):
     return {
         "message": "Unauthorized",
     }
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
