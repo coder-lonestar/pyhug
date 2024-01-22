16eb4074140e713df9e52bf1a194a8375fc29b62
BEAST\Prashanth <nagaprashanth0006@gmail.com>
2024-01-22 14:43:44 +0530

    Updated GET param syntax

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import hug
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 import os
 import base64
 import logging
@@ -11,14 +11,17 @@ def authenticated(key):
     salt = base64.b64decode(str.encode(os.environ["PYHUG_SALT"].strip('"'))).decode()
     password = base64.b64encode(str.encode(salt + key.strip('"'), "utf-8")).decode()
     matching_hash = os.environ["PYHUG_HASH"].strip('"')
+
     if password == matching_hash:
         return True
     return False
 
 
-@app.route("/")
-def greet(name: str = "dummy", key: str = "fake"):
+@app.route("/", methods=["GET"])
+def greet():
     """Greets user"""
+    name = request.args.get("name", default="dummy", type=str)
+    key = request.args.get("key", default="fake", type=str)
     if authenticated(key):
         return {"message": "Hello {0}. Have a nice day!".format(name)}
     return {
